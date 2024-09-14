525edd1ee60347d313e713ff69c6d6b7f16e7e10
Podisuduu02 <154498962+Podisuduu02@users.noreply.github.com>
2024-09-14 19:39:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=yEsCUKKS#eVT3WEOSZgyOxprWaD_fRe192AMuCTDJrTpRbzolLLE' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=zUMTzSQb#1F2J1owmsLjb__z4bljSo1BdBn6yk7QVyxbSiB1GClc' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+945262620' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
